1327423b7981b90dc43376fef1e01eeb62673ba4
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 14:53:20 -0600

    Remove unused imports and clean up line spacing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../Header/Header'
 import Plants from '../../containers/Plants/Plants'
-import { fetchPlants } from '../../apiCalls'
-
 import './App.scss';
 
 const App = () => {
-
-    return (
-      <main className="App">
-        <Header />
-        <Plants />
-      </main>
-    )
-   
+  return (
+    <main className="App">
+      <Header />
+      <Plants />
+    </main>
+  )
 }
 
 export default App;
