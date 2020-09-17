cd9a20adf0e15a27c0b10144e2be75c491ad85bc
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 09:09:26 -0600

    Remove bindActionCreators and dispatch argument from mapDispatchToProps

---
+++ src/containers/Plants/Plants.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import PlantCard from '../../components/PlantCard/PlantCard'
+import { fetchEdibleRoots, fetchEdibleFlowers } from '../../apiCalls'
+import { connect } from 'react-redux'
+import { setEdibleFlowers, setEdibleRoots } from '../../actions';
+import './Plants.scss';
+
+
+class Plants extends Component {
+  constructor() {
+    super()
+    this.state = {
+      error: '',
+    } 
+  }
+
+  async componentDidMount() {
+    const { setEdibleRoots, setEdibleFlowers } = this.props;
+    let roots = await fetchEdibleRoots()
+    let flowers = await fetchEdibleFlowers()
+    setEdibleFlowers(flowers)
+    setEdibleRoots(roots)
+  }
+
+// filterPlants (input)
+// if input matches listName(s) return listname 
+// allplants
+// roots
+// flowers
+
+
+  render() {
+    const { roots, flowers } = this.props;
+    const plants = roots.concat(flowers)
+    console.log('combined lists', plants)
+    
+    const plantInfo = plants.map(plant => {
+     return <PlantCard 
+        key={plant.id}
+        id={plant.id}
+        plantName={plant.common_name}
+        image={plant.image_url}
+        sciName={plant.scientific_name}
+      />
+    })
+
+    return (
+      <section className='Plants'>
+        <h1 className='page-heading'>Browse Plants</h1>
+        <section className='plant-container'>
+          {plantInfo}
+        </section>
+      </section>
+    )
+  }
+}
+
+export const mapStateToProps = ({ roots, flowers }) => ({
+  roots,
+  flowers
+})
+
+export const mapDispatchToProps = {
+    setEdibleRoots,
+    setEdibleFlowers
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps) (Plants)
