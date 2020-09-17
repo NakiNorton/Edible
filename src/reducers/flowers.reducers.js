4f80b8aecbb4559006bd4209b60149ba0b8a0f79
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 14:55:51 -0600

    Delete unused files

--- src/reducers/flowers.reducers.js
+++
@@ -1,9 +0,0 @@
-export const flowers = (state = [], action) => {
-  switch (action.type) {
-    case 'SET_EDIBLE_FLOWERS':
-      const newState = [...state, ...action.flowers]
-      return newState
-    default:
-      return state
-  }
-}
