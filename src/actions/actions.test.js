365f29469498395ce1db454ba3c28cc0fe27712d
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 08:27:42 -0600

    Refactor tests to work with refactored implementation code

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,14 +1,26 @@
 import * as actions from '../actions';
 
 describe('actions', () => {
-  it('should have a type of SET_PLANTS', () => {
-    const plants = [1, 2, 3];
+  it('should have a type of SET_EDIBLE_ROOTS', () => {
+    const roots = [1, 2, 3];
     const expectedAction = {
-      type: 'SET_PLANTS',
-      plants: [1, 2, 3]
+      type: 'SET_EDIBLE_ROOTS',
+      roots: [1, 2, 3]
     }
 
-    const result = actions.setPlants(plants);
+    const result = actions.setEdibleRoots(roots);
+
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of SET_EDIBLE_flowers', () => {
+    const flowers = [1, 2, 3];
+    const expectedAction = {
+      type: 'SET_EDIBLE_FLOWERS',
+      flowers: [1, 2, 3]
+    }
+
+    const result = actions.setEdibleFlowers(flowers);
 
     expect(result).toEqual(expectedAction);
   });
