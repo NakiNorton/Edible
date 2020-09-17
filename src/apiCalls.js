62d6b0efa091b86ce3c7f33a56ceb97d7297ae30
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 07:05:28 -0600

    Write and pass test for rendering correct Header UI

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,24 @@
 const key = process.env.REACT_APP_API_KEY;
 
-export async function fetchPlants() {
+export async function fetchEdibleRoots() {
   const response = await fetch('https://fe-cors-proxy.herokuapp.com', {
     headers: {
-      "Target-URL": `https://trefle.io/api/v1/plants?token=${key}&filter[edible_part]=roots,flowers`
+      "Target-URL": `https://trefle.io/api/v1/plants?token=${key}&filter[edible_part]=roots`
     }
   })
-  const plants = await checkResponse(response);
-  return plants.data;
+  const edibleRoots = await checkResponse(response);
+  console.log('api', edibleRoots.data)
+  return edibleRoots.data;  
+}
+
+export async function fetchEdibleFlowers() {
+  const response = await fetch('https://fe-cors-proxy.herokuapp.com', {
+    headers: {
+      "Target-URL": `https://trefle.io/api/v1/plants?token=${key}&filter[edible_part]=flowers`
+    }
+  })
+  const edibleFlowers = await checkResponse(response);
+  return edibleFlowers.data;
 }
 
 const checkResponse = async (response) => {
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { screen, render } from '@testing-library/react'
+import Header from './Header'
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Header component', () => {
+  it('should display the correct information on page load', () => {
+
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    )
+
+    const homeLink = screen.getByText('HOME')
+    const savedPlantsLink = screen.getByText('SAVED PLANTS')
+
+    expect(homeLink).toBeInTheDocument();
+    expect(savedPlantsLink).toBeInTheDocument();
+  })
+})
