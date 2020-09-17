4e9cb7a0c950ea5e642c0f7ee449e495d1020dcb
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 12:15:30 -0600

    Add plantSaved and list properties to object

--- src/cleanData.js
+++ src/cleanData.js
@@ -1,10 +1,12 @@
-export const cleanData = (plants) => {
-  const filteredData = plants.map(plant => {
-    return {
+export const cleanData = (plants, list) => {
+  let filteredData = plants.map(plant => {
+   return {  
       common_name: plant.common_name,
       scientific_name: plant.scientific_name,
       id: plant.id + Date.now(),
-      image_url: plant.image_url
+      image_url: plant.image_url,
+      list: list,
+      plantSaved: false,
     }
   })
   return filteredData
