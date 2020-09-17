64aed30d85aeb3fb73bc2e3e3dd3d8944d65959e
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 13:15:51 -0600

    Refactor test to work with new plant reducers structure

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,18 +1,10 @@
-import { roots } from './roots.reducers';
-import { flowers } from './flowers.reducers';
+import { plants } from './plants.reducers';
 
 describe('Plants Reducer', () => {
   it('Should return the initial state for roots', () => {
 
     const expected = [];
-    const result = roots(undefined, {});
-
-    expect(result).toEqual(expected)
-  })
-
-  it('Should return the initial state for flowers', () => {
-    const expected = [];
-    const result = flowers(undefined, {});
+    const result = plants(undefined, {});
 
     expect(result).toEqual(expected)
   })
