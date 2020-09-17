7c3d12f3d5ce7a40e65d3d5b710397ba3941d9d5
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-17 08:32:10 -0600

    Add roots and flowers to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
-import { plants } from './plants.reducers'
+import { roots } from './roots.reducers'
+import { flowers } from './flowers.reducers'
 
 export const rootReducer = combineReducers({
-  plants
+  roots,
+  flowers
 })
