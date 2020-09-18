6954a0a06a8f0008bda55a2933fdc3b0c973d3bc
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-18 12:06:03 -0600

    Delete tests for deleted actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,10 +1,10 @@
 import * as actions from '../actions';
 
 describe('actions', () => {
-  it('should have a type of SET_EDIBLE_ROOTS', () => {
+  it('should have a type of SET_EDIBLE_PLANTS', () => {
     const plants = [1, 2, 3];
     const expectedAction = {
-      type: 'SET_EDIBLE_ROOTS',
+      type: 'SET_EDIBLE_PLANTS',
       plants: [1, 2, 3]
     }
 
@@ -12,25 +12,4 @@ describe('actions', () => {
     expect(result).toEqual(expectedAction);
   });
 
-  it('should have a type of SET_EDIBLE_FLOWERS', () => {
-    const plants = [1, 2, 3];
-    const expectedAction = {
-      type: 'SET_EDIBLE_FLOWERS',
-      plants: [1, 2, 3]
-    }
-
-    const result = actions.setEdibleFlowers(plants);
-    expect(result).toEqual(expectedAction);
-  });
-
-  it('should have a type of SET_EDIBLE_LEAVES', () => {
-    const plants = [1, 2, 3];
-    const expectedAction = {
-      type: 'SET_EDIBLE_LEAVES',
-      plants: [1, 2, 3]
-    }
-
-    const result = actions.setEdibleLeaves(plants);
-    expect(result).toEqual(expectedAction);
-  });
 });
