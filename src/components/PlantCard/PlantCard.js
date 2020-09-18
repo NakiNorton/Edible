2ca92ec6cd3f33e68fb68f7cef50bbf9840374ba
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-18 16:55:27 -0600

    Add save plant button to plant cards

--- src/components/PlantCard/PlantCard.js
+++ src/components/PlantCard/PlantCard.js
@@ -10,7 +10,8 @@ const PlantCard = ({ id, plantName, image, sciName, list }) => {
         <article id={id} className='plant-details'>
           <p>Common name: {plantName}</p>
           <p>Scientific name: {sciName}</p> 
-          <p>Edible part: {list}</p> 
+          <p>What's edible? The {list}</p> 
+          <button className='save-plant'>Save</button>
         </article>
     </section>
   )
