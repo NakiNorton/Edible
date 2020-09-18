05c3aa01b534a6e3a8b71c6db08038f3aa3d43cf
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-18 16:41:08 -0600

    Add savedPlants and router on plants and savedPlants to enable navigate around app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../Header/Header'
 import Plants from '../../containers/Plants/Plants'
+import SavedPlants from '../../containers/SavedPlants/SavedPlants'
 import './App.scss';
 
 const App = () => {
   return (
     <main className="App">
       <Header />
-      <Plants />
+      <Route exact path='/' render={() =>  
+        <Plants />
+      } />
+      <Route exact path='/saved-plants' render={() =>  
+        <SavedPlants />
+      } />
+
     </main>
   )
 }
