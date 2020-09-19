ac41179218923ee3fffba42421b3541bc31379f2
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-19 07:52:58 -0600

    Create reduced to change plandSaved value to true when action is fired

--- src/reducers/plants.reducers.js
+++ src/reducers/plants.reducers.js
@@ -3,6 +3,20 @@ export const plants = (state = [], action) => {
     case 'SET_EDIBLE_PLANTS':
       const newState = action.plants
       return newState
+    case 'ADD_PLANT':
+      const modifiedState = state
+      modifiedState.filter(plant => {
+        if(plant.id === action.plants) {
+         return plant.plantSaved = true;
+        } 
+      })
+      return modifiedState
+
+
+
+    case 'REMOVE_PLANT':
+      const removePlant = action.plants
+      return removePlant
     default:
       return state
   }
