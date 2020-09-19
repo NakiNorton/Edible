988ee6235a8a2a221bfeddecb1388b7984ac9bd5
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-19 10:28:15 -0600

    Change common name to a heading

--- src/containers/PlantCard/PlantCard.js
+++ src/containers/PlantCard/PlantCard.js
@@ -6,18 +6,20 @@ import './PlantCard.scss';
 const PlantCard = ({ id, plantName, image, sciName, list, isSaved, addPlant, removePlant }) => {
 
   const handleClick = () =>  {
-  !isSaved ? addPlant(id) : removePlant(id)
+    !isSaved ? addPlant(id) : removePlant(id)
   }
 
   return (
     <section className='PlantCard'>
-        <img className='Book-card-image' alt={plantName} src={image} />
-        <article id={id} className='plant-details'>
-          <p>Common name: {plantName}</p>
-          <p>Scientific name: {sciName}</p> 
-          <p>What's edible? The {list}</p> 
-          <button className='save-plant-btn' onClick={handleClick}>{!isSaved ? 'Save' : 'Remove'}</button>
-        </article>
+      <h2 className='common-name'>{plantName}</h2>
+      <img className='Book-card-image' alt={plantName} src={image} />
+      <article id={id} className='plant-details'>
+        <p>Scientific name: {sciName}</p> 
+        <p>What's edible? The {list}</p> 
+        <button className='save-plant-btn' onClick={handleClick}>
+          {isSaved ? 'Saved' : 'Save'}
+        </button>
+      </article>
     </section>
   )
 }
