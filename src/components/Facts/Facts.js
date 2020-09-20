47f466071e2dc3d204a566ea22fd5fa83d644d23
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-19 19:02:15 -0600

    Add Facts component and styling

---
+++ src/components/Facts/Facts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Facts.scss';
+
+const Facts  = () => {
+  return (
+    <div className='Facts'>
+      <p>'Even in the modern world, it’s possible to find and pick wild plants that are not just edible, but highly nutritious.'</p>
+      <p>'Edible plants found in the wild are often more nutritious than the kinds you can buy at the store.'</p>
+      <p>'The carbon footprint of wild greens virtually nil.'</p>
+    </div>
+  )
+}
+
+export default Facts
