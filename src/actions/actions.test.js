cffbc4167d934c13c6340cea1d6147d0d5446dd4
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-20 07:54:49 -0600

    Write action tests for adding and removing actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,13 +2,68 @@ import * as actions from '../actions';
 
 describe('actions', () => {
   it('should have a type of SET_EDIBLE_PLANTS', () => {
-    const plants = [1, 2, 3];
+    const plants = [
+      {
+        id: 10978,
+        common_name: 'garden ginger',
+        scientific_name: 'Zingiber officinale',
+        image_url: 'https://bs.floristic.org/image/o/bd13',
+        list: 'leaves',
+        plantSaved: false
+      },
+      {
+        id: 19630,
+        common_name: 'lawndaisy',
+        scientific_name: 'Bellis perennis',
+        image_url: 'https://bs.floristic.org/image/o/430',
+        list: 'leaves',
+        plantSaved: false
+      }
+    ]
     const expectedAction = {
       type: 'SET_EDIBLE_PLANTS',
-      plants: [1, 2, 3]
+      plants: plants 
     }
 
     const result = actions.setEdiblePlants(plants);
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of ADD_PLANT', () => {
+    const plants = {
+      id: 10978,
+      common_name: 'garden ginger',
+      scientific_name: 'Zingiber officinale',
+      image_url: 'https://bs.floristic.org/image/o/bd13',
+      list: 'leaves',
+      plantSaved: false
+    }
+
+    const expectedAction = {
+      type: 'ADD_PLANT',
+      plants: plants
+    }
+
+    const result = actions.addPlant(plants);
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of REMOVE_PLANT', () => {
+    const plants = {
+      id: 10978,
+      common_name: 'garden ginger',
+      scientific_name: 'Zingiber officinale',
+      image_url: 'https://bs.floristic.org/image/o/bd13',
+      list: 'leaves',
+      plantSaved: false
+    }
+
+    const expectedAction = {
+      type: 'REMOVE_PLANT',
+      plants: plants
+    }
+
+    const result = actions.removePlant(plants);
     expect(result).toEqual(expectedAction);
   });
 
