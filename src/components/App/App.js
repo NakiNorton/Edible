d40cd9d6ffb0c7c72d8037a0dfc35f13a8a38b46
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-21 12:23:35 -0600

    Remove unused plants prop and clean up render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { fetchEdiblePlants } from '../../helpers/apiCalls'
 import { setEdiblePlants } from '../../actions';
 import Header from '../Header/Header'
@@ -14,7 +13,7 @@ class App extends Component {
   state = { isLoading: true }
 
   async componentDidMount() {
-    const { setEdiblePlants } = this.props;
+    const { setEdiblePlants } = this.props
     try {
       const leavesData = await fetchEdiblePlants('leaves')
       const rootsData = await fetchEdiblePlants('roots')
@@ -35,42 +34,29 @@ class App extends Component {
     }
   }
 
-
   render() {
     return (
       <main className='App'>
         <Header />
         {this.state.isLoading &&
           <>
-          <h1 className='page-loading'>Page loading...</h1>
-          <img className='plant-img' alt='plant' src={plantImg} />
+            <h1 className='page-loading'>Page loading...</h1>
+            <img className='plant-img' alt='plant' src={plantImg} />
           </>
         }
-        {!this.state.isLoading &&
-          <>
-          <Route exact path='/' render={() =>  
-            <Plants />
-          } />
-          <Route exact path='/saved-plants' render={() =>
-            <SavedPlants />
-          } />
-          </>
-        }
+        <Route exact path='/' render={() =>  
+          <Plants />
+        } />
+        <Route exact path='/saved-plants' render={() =>
+          <SavedPlants />
+        } />
       </main>
     )
   }
 }
 
-App.propTypes = {
-  plants: PropTypes.array
-}
-
-export const mapStateToProps = ({ plants }) => ({
-  plants
-})
-
 export const mapDispatchToProps = {
   setEdiblePlants
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(null, mapDispatchToProps)(App)
